ba4370e2617958dc62ff051525ad8d692cad575c
Jaime Gomez <gomezv9@hotmail.com>
2018-02-18 15:53:12 -0500

    remove hover dropdown and add event click dropdown

--- src/app/components/header/header.controller.js
+++ src/app/components/header/header.controller.js
@@ -6,10 +6,36 @@
     	.controller('headerController', function(){
     		  var vm = this;
 
-		      jQuery('#nav-icon2').click(function(){
-		          jQuery(this).toggleClass('open');
-		          jQuery( "#menuMobile2" ).fadeToggle("fast" );
+              /* Menu Hamburger */
+
+		      $('#nav-icon2').click(function(){
+		          $(this).toggleClass('open');
+		          $( "#menuMobile2" ).fadeToggle("fast" );
 		      });
+
+              /*******************/
+
+              /* Dropdown in header */
+
+              $(document).on("click", function () {
+                  $(".dropdown-content").hide();
+              });
+
+              $(".dropdown-content").on("click", function (event) {
+                  event.stopPropagation();
+              });
+
+              $(".browserCategory").click(function(event){
+                  event.stopPropagation();
+                  $(this).children('ul').slideToggle();
+              })
+
+              $(".iconArrow").click(function(event){
+                  event.stopPropagation();
+                  $(".dropdown-content").slideToggle();
+              })
+
+              /**************************/
 			
     	});
 
